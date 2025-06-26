10ea573b0d8a7c45bdf9e79d7d5b07252800c38a
bunta1009 <ff7kulaudo@gmail.com>
2025-06-26 22:19:57 +0900

    Delete portfolio directory

--- portfolio/js/5-17.js
+++
@@ -1,5 +0,0 @@
-$("#particle").particleText({
-  text: "Welcome to Bunta’s Portfolio", // 表示させたいテキスト。改行の場合は<br>追加
-  colors:["#111"], // パーティクルの色を複数指定可能
-  speed: "high", // slow, middle, high の3つから粒子が集まる速さを選択
-  });
